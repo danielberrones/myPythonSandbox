69a98d366a1849b20da76a9b11d14b3603ac8d90
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-06-10 08:50:34 -0500

    Updated File

---
+++ main.py
@@ -0,0 +1,24 @@
+class Student:
+    def __init__(self,name,qhours,qpoints):
+        self.name=name
+        self.qhours=qhours
+        self.qpoints=qpoints
+    def getName(self):
+        return self.name
+    def getQualityHours(self):
+        return self.qhours
+    def getQualityPoints(self):
+        return self.qpoints
+    def gpa(self):
+        return self.qpoints/self.qhours
+
+
+def main():
+    aStudent = Student("Mark Williams",33,39)
+    print(aStudent.getName())
+    print(aStudent.getQualityHours())
+    print(aStudent.getQualityPoints())
+    print(aStudent.gpa())
+
+
+main()
