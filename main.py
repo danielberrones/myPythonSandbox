7e7819fe9a28dab19527382de7af6df58b895fa0
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-06-10 08:53:46 -0500

    Updated File

--- main.py
+++ main.py
@@ -15,10 +15,19 @@ class Student:
 
 def main():
     aStudent = Student("Mark Williams",33,89)
+    bStudent = Student("Jeb Doe",66,150)
+
     print(aStudent.getName())
+    print(bStudent.getName())
+
     print(aStudent.getQualityHours())
+    print(bStudent.getQualityHours())
+
     print(aStudent.getQualityPoints())
+    print(bStudent.getQualityPoints())
+
     print(aStudent.gpa())
+    print(bStudent.gpa())
 
 
 main()
