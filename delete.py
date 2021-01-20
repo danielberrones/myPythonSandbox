eebf4ee866e3472c2b7275a06876e2c7430cdece
danielberrones <daniel.a.berrones@gmail.com>
2021-01-19 20:24:57 -0600

    Refactored Code

--- delete.py
+++ delete.py
@@ -2,16 +2,16 @@ import bs4, requests
 
 url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
 
-def returnReqObj(url):
+def scrapeMovies(url):
+    '''Scrape IMBDB Top 250 Movies'''
     r = requests.get(url)
     soup = bs4.BeautifulSoup(r.text,'lxml')
-    return soup
 
-soup = returnReqObj(url)
+    tableData = [i.get_text() for i in soup.find_all('td', class_='titleColumn')]
+    
+    with open('file1.txt','w') as f:
+        for i in td:
+            f.write(str(i.strip('/n')))
 
-td = [i.get_text() for i in soup.find_all('td', class_='titleColumn')]
 
-for i in td:
-        print(i.strip('/n'))
-
-        
+scrapeMovies(url)
